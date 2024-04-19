28f0ff7108048dfead712e5b5af753b467337ee3
Sula <turatbekovchannel2018@gmail.com>
2024-04-20 04:17:08 +0600

    fix bugs

--- src/routes/Student.tsx
+++ src/routes/Student.tsx
@@ -3,6 +3,7 @@ import {NotFoundPage} from "@/src/pages/NotFoundPage/NotFoundPage.tsx";
 import {StudentRoutes} from "@/src/routes/StudentRoutes.tsx";
 import {StudentDashboard} from "@/src/pages/Student/StudentDashboard.tsx";
 import {StudentLogin} from "@/src/pages/Student/StudentLogin.tsx";
+import {StudentRegistration} from "@/src/components/shared/StudentRegistration/StudentRegistration.tsx";
 
 export const Student = () => {
     return (
@@ -10,6 +11,7 @@ export const Student = () => {
             <Route path="/" element={<StudentRoutes />}>
                 <Route path='' element={<StudentDashboard />} />
                 <Route path="login" element={<StudentLogin />} />
+                <Route path="registration" element={<StudentRegistration />} />
                 <Route path="*" element={<NotFoundPage />} />
             </Route>
         </Routes>
